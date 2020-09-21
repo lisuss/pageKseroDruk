12f1c98e21e78f7106a1328213b6221db674a053
lisuss <55858498+lisuss@users.noreply.github.com>
2020-09-21 18:16:43 +0200

    Starting Point

---
+++ kserodruk-site/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    Hello
+    <router-outlet></router-outlet>
+  `,
+  styles: []
+})
+export class AppComponent {
+  title = 'kserodruk-site';
+}
